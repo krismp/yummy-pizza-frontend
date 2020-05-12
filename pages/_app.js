1cc6610aea03d1174f9d7e213eac895be4353fa1
krismp <kris.mp7777@gmail.com>
2020-05-13 06:12:36 +0700

    Configure material-ui ssr

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,50 @@
-import App from 'next/app'
-import React from 'react'
-import withReduxStore from '../lib/with-redux-store'
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
+import App from "next/app";
+import React from "react";
+import withReduxStore from "../lib/with-redux-store";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
-class MyApp extends App {
-  constructor(props) {
-    super(props)
-    this.persistor = persistStore(props.reduxStore)
-  }
+function MyApp(props) {
+  const { Component, pageProps, reduxStore } = props;
 
-  render() {
-    const { Component, pageProps, reduxStore } = this.props
-    return (
-      <Provider store={reduxStore}>
-        <PersistGate
-          loading={<Component {...pageProps} />}
-          persistor={this.persistor}
-        >
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    )
-  }
+  const persistor = persistStore(reduxStore)
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <Provider store={reduxStore}>
+      <PersistGate
+        loading={<Component {...pageProps} />}
+        persistor={persistor}
+      >
+        <React.Fragment>
+          <Head>
+            <title>My page</title>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width"
+            />
+          </Head>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </React.Fragment>
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default withReduxStore(MyApp)
+export default withReduxStore(MyApp);
