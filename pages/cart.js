61e5e5913a73df0dcf6ac78a6fb2dfd913329e2f
krismp <kris.mp7777@gmail.com>
2020-05-16 16:03:05 +0700

    Improve cart page experience

--- pages/cart.js
+++ pages/cart.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Layout from "../components/layout";
 import CartItem from "../components/CartItem";
 import fetch from "isomorphic-unfetch";
@@ -37,12 +38,14 @@ export function CartPage(props) {
 
   const fetchData = async () => {
     if (props.cartId) {
+      setLoading(true);
       const result = await fetch(
         `${publicRuntimeConfig.API_BASE_URL}/carts/${props.cartId}`,
       );
   
       const json = await result.json();
       setCart(json.data);
+      setLoading(false);
     }
   }
 
@@ -87,57 +90,51 @@ export function CartPage(props) {
     }
   }
 
-  if (cart.cart_items.length > 0) {
-    return <Layout>
-      <Typography gutterBottom variant="h5" component="h2">
-        Your Cart
-      </Typography>
-      <Grid
-        container
-        direction="row"
-        justify="flex-end"
-        alignItems="center"
-        spacing={2}
-      >
-        <Grid item xs={12}>
-          {cart.cart_items.length > 0 &&
-            cart.cart_items.map((item) => {
-              return (
-                <CartItem
-                  key={item.id}
-                  itemId={item.id}
-                  total={item.total}
-                  total_price_in_usd={item.total_price_in_usd}
-                  product={item.product}
-                  onDeleteItem={handleDeleteCartItem}
-                />
-              );
-            })}
+  return <Layout>
+    <Typography gutterBottom variant="h5" component="h2">
+      Your Cart
+    </Typography>
+    <Grid
+      container
+      direction="row"
+      justify="flex-end"
+      alignItems="center"
+      spacing={2}
+    >
+      <Grid item xs={12}>
+        {!loading && cart.cart_items.length > 0 ?
+          cart.cart_items.map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                itemId={item.id}
+                total={item.total}
+                total_price_in_usd={item.total_price_in_usd}
+                product={item.product}
+                onDeleteItem={handleDeleteCartItem}
+              />
+            );
+          }) : (loading ? <CircularProgress/> : <Typography gutterBottom variant="h5" component="h2">
+            Your cart is empty!
+          </Typography>)
+        }
+      </Grid>
+    </Grid>
+    <TotalCartPrice elevation={6}>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={6}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Total: ${cart.total_price}
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" color="primary" onClick={handleCheckout}>
+            {props.isLoggedIn ? `Checkout` : `Checkout as Guest` }
+          </Button>
         </Grid>
       </Grid>
-      <TotalCartPrice elevation={6}>
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Grid item xs={6}>
-            <Typography gutterBottom variant="h5" component="h2">
-              Total: ${cart.total_price}
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Button variant="contained" color="primary" onClick={handleCheckout}>
-              {props.isLoggedIn ? `Checkout` : `Checkout as Guest` }
-            </Button>
-          </Grid>
-        </Grid>
-      </TotalCartPrice>
-    </Layout>;
-  }
-  return (
-    <Layout>
-      <Typography gutterBottom variant="h5" component="h2">
-        Your cart is empty!
-      </Typography>
-    </Layout>
-  )
+    </TotalCartPrice>
+  </Layout>;
 }
 
 function mapStateToProps(state) {
