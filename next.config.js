b34b1d115541fc3a4c944cce1b5427c7986a5f9c
krismp <kris.mp7777@gmail.com>
2020-05-17 13:03:48 +0700

    Configure bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   publicRuntimeConfig: {
     // Will only be available on the server side
     API_BASE_URL: process.env.API_BASE_URL,
-  },
-};
+  }
+});
